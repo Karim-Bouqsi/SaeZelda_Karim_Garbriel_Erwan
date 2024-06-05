21dbc88510ab45e8acfbf48d136cde9c6ea0dff6
b0uman <narutovignon@gmail.com>
2024-06-06 01:12:28 +0200

    modification du fichier fxml et creation de la classe potion

---
+++ src/main/java/sae/saezelda/modele/Potion.java
@@ -0,0 +1,7 @@
+package sae.saezelda.modele;
+
+public class Potion extends Item{
+    public Potion(String nom){
+        super(nom);
+    }
+}
---
+++ src/main/java/sae/saezelda/modele/PotionVie.java
@@ -0,0 +1,10 @@
+package sae.saezelda.modele;
+
+public class PotionVie extends Potion {
+    int pv;
+    public PotionVie(){
+        super("Potion de vie");
+        this.pv=20;
+    }
+
+}
